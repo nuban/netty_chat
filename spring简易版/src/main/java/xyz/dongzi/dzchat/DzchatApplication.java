51285cda20b44604878bce868521cc34b6a8114b
wendong <2911890696@qq.com>
2022-05-16 17:15:29 +0800

    F最基础的netty入门

---
+++ spring简易版/src/main/java/xyz/dongzi/dzchat/DzchatApplication.java
@@ -0,0 +1,13 @@
+package xyz.dongzi.dzchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DzchatApplication.class, args);
+    }
+
+}
---
+++ spring简易版/src/main/java/xyz/dongzi/dzchat/netty/WebSocketNettyServer.java
@@ -0,0 +1,41 @@
+package xyz.dongzi.dzchat.netty;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class WebSocketNettyServer {
+    public static void main(String[] args) {
+        // 创建两个线程池
+        NioEventLoopGroup bosses = new NioEventLoopGroup();
+        NioEventLoopGroup workers = new NioEventLoopGroup();
+
+        try {
+            // 创建Netty服务器启动对象
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            // 初始化服务器启动对象
+            serverBootstrap
+                    // 指定使用上面创建的两个线程池
+                    .group(workers, workers)
+                    // 指定Netty通道类型
+                    .channel(NioServerSocketChannel.class)
+                    // 指定通道初始化器用来加载当Channel收到事件消息后，
+                    // 如何进行业务处理
+                    .childHandler(new WebSocketChannelInitializer());
+
+            // 绑定服务器端口，以同步的方式启动服务器
+            ChannelFuture future = serverBootstrap.bind(8080).sync();
+            // 等待服务器关闭
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // 优雅关闭服务器
+            workers.shutdownGracefully();
+            bosses.shutdownGracefully();
+        }
+
+    }
+}
+
